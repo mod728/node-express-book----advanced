5ee9c5162c9b59e9c358e6dd8e4c2d0254d1916c
mo728 <phallus-phaser@hotmail.com>
2024-09-08 12:17:54 +0200

    Code for Chapter 5

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+app.use(express.urlencoded({ extended: true })) 
+const mongoose = require("mongoose")  
+const session = require("express-session") 
+const routers = require("./routes") 
+
+app.set("view engine", "ejs") 
+app.use("/public", express.static("public"))
+
+// Session
+app.use(session({
+    secret: "secretKey",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 3600000},
+}))
+
+// Connecting to MongoDB
+mongoose.connect("mongodb+srv://xxxxxxx:yyyyyyyy@cluster0.xycrj.mongodb.net/blogUserDatabase?retryWrites=true&w=majority&appName=Cluster0") 
+    .then(() => {
+        console.log("Success: Connected to MongoDB")
+    })
+    .catch((error) => {
+        console.error("Failure: Unconnected to MongoDB")
+    })
+
+app.use(routers) 
+
+// Page NotFound
+app.get("*", (req, res) => {
+    res.render("error", {message: "ページが存在しません"})
+})
+
+// Connecting to port
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {        
+    console.log(`Listening on ${port}`)
+})
